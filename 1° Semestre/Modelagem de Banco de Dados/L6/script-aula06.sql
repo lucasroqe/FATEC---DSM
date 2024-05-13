fc6958d6d34cefce9af9a6909c7a4744e59b2ede
lucasroqe <alvim.lucas2@hotmail.com>
2024-05-13 10:27:08 -0300

    Adiciona exercícios SQL

---
+++ 1° Semestre/Modelagem de Banco de Dados/L6/script-aula06.sql
@@ -0,0 +1,29 @@
+--ex1
+SELECT * FROM tbfaculdade;
+
+--ex2
+SELECT * FROM tbfaculdade ORDER BY municipio ASC;
+
+--ex3
+SELECT * FROM tbfaculdade WHERE municipio='Jacareí';
+
+--ex4
+SELECT * FROM tbfaculdade WHERE numero is NULL;
+
+--ex5
+SELECT * FROM tbfaculdade WHERE municipio LIKE 'f%';
+
+--ex6
+SELECT * FROM tbfaculdade WHERE bairro LIKE '%vila%';
+
+--ex7
+SELECT * FROM tbfaculdade WHERE cep LIKE '%000';
+
+--ex8
+SELECT * from tbfaculdade WHERE cep like '_5%';
+
+--ex9
+SELECT * FROM tbfaculdade WHERE cep LIKE '_5%2_';
+
+--ex10
+SELECT * FROM tbfaculdade WHERE numero LIKE '__';
---
+++ 1° Semestre/Modelagem de Banco de Dados/L6/script-aula07.sql
@@ -0,0 +1,38 @@
+--ex1
+SELECT unidade AS 'Fatec', COUNT(*) AS 'Quantidade' FROM tbcurso GROUP BY unidade ORDER BY unidade ASC;
+
+
+--ex2
+SELECT unidade AS 'Fatec', COUNT(*) AS 'Quantidade' FROM tbcurso GROUP BY unidade ORDER BY COUNT(*) DESC;
+
+
+--ex3
+SELECT unidade AS 'Fatec', COUNT(*) AS 'Quantidade'FROM tbcurso GROUP BY unidade ORDER BY COUNT(*) DESC LIMIT 1;
+
+
+--ex4
+SELECT unidade AS 'Fatec', COUNT(*) AS 'Quantidade'FROM tbcurso GROUP BY unidade ORDER BY COUNT(*) DESC LIMIT 1 OFFSET 1;
+
+
+--ex5
+SELECT unidade AS 'Fatec', COUNT(*) AS 'Quantidade' FROM tbcurso GROUP BY unidade HAVING COUNT(*) = 3 ORDER BY unidade ASC;
+
+
+--ex6
+SELECT unidade AS 'Fatec', turno, COUNT(*) AS 'Quantidade' FROM tbcurso GROUP BY unidade, turno ORDER BY unidade, turno ASC;
+
+
+--ex7
+SELECT unidade AS 'Fatec', turno, COUNT(*) AS 'Quantidade' FROM tbcurso GROUP BY unidade, turno HAVING COUNT(*) = 5 ORDER BY unidade, turno ASC;
+
+
+--ex8
+SELECT unidade AS 'Fatec', turno, COUNT(*) AS 'Quantidade' FROM tbcurso WHERE unidade LIKE '%São José dos Campos%' GROUP BY unidade, turno ORDER BY unidade, turno ASC;
+
+
+--ex9
+SELECT unidade AS 'Fatec', turno, SUM(vaga) AS 'Quantidade' FROM tbcurso WHERE unidade LIKE '%São José dos Campos%' GROUP BY unidade, turno ORDER BY unidade, turno ASC;
+
+
+--ex10
+SELECT turno, SUM(vaga) AS 'Quantidade Soma' FROM tbcurso GROUP BY turno;
